8779b87f5873d74d0051d05c83f091dbbc2cfeb5
phillip bligh <pbligh@ashly.com>
2021-01-23 01:20:26 -0500

    make gamedraw.py work on mac and linux, update readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # ZeroMQ-Example
+
+This project contains simple publish subscribe examples for zeromq. The publisher is implimented in Node javascript and the subscriber is implimented in Node Javacript and in Python.
+
+
+
+
+You will need the ZeroMQ
+npm install zeromq
+
+
+#
--- gamefiles/gamedraw.py
+++ gamefiles/gamedraw.py
@@ -1,5 +1,21 @@
-import os
-clear = lambda: os.system('cls')
+
+# import only system from os 
+from os import system, name 
+  
+# import sleep to show output for some time period 
+from time import sleep 
+  
+# define our clear function 
+def clear(): 
+  
+    # for windows 
+    if name == 'nt': 
+        _ = system('cls') 
+  
+    # for mac and linux(here, os.name is 'posix') 
+    else: 
+        _ = system('clear') 
+
 
 def gamedraw(gamedata):
     ball = gamedata['ball']
